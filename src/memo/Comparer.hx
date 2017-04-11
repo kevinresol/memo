d9a681f10ab659872bd13c307d696a9436b29cd9
Kevin Leung <kevinresol@gmail.com>
2017-04-11 20:00:52 +0800

    Add PairComparer

--- src/memo/Comparer.hx
+++ src/memo/Comparer.hx
@@ -1,5 +1,7 @@
 package memo;
 
+using tink.CoreApi;
+
 @:forward
 abstract Comparer<T>(ComparerObject<T>) from ComparerObject<T> to ComparerObject<T> {
 	@:from
@@ -18,6 +20,19 @@ class FuncComparer<T> implements ComparerObject<T> {
 		this.f = f;
 	public function equals(v1:T, v2:T):Bool
 		return f(v1, v2);
+}
+
+class PairComparer<T1, T2> implements ComparerObject<Pair<T1, T2>> {
+	static var inst:PairComparer<Dynamic, Dynamic> = new PairComparer(EqualityComparer.get(), EqualityComparer.get());
+	public static function get<T1, T2>():PairComparer<T1, T2> return cast inst;
+	var c1:Comparer<T1>;
+	var c2:Comparer<T2>;
+	public function new(c1, c2) {
+		this.c1 = c1;
+		this.c2 = c2;
+	}
+	public function equals(v1:Pair<T1, T2>, v2:Pair<T1, T2>):Bool
+		return c1.equals(v1.a, v2.a) && c2.equals(v1.b, v2.b);
 }
 
 class EqualityComparer<T> implements ComparerObject<T> {
--- src/memo/Memo.hx
+++ src/memo/Memo.hx
@@ -9,14 +9,23 @@ abstract Memo<In, Out>(MemoObject<In, Out>) from MemoObject<In, Out> to MemoObje
 		this = new SimpleMemo(f, comparer);
 	
 	@:from
-	public static function ofNext<In, Out>(f:Next<In, Out>):Memo<In, Out>
+	public static inline function ofNext<In, Out>(f:Next<In, Out>):Memo<In, Out>
 		return new Memo(f);
 		
-	public function map<A>(f:Next<Out, A>, ?comparer:Comparer<Out>):Memo<In, A>
+	public inline function map<A>(f:Next<Out, A>, ?comparer:Comparer<Out>):Memo<In, A>
 		return new MapMemo(this, f, comparer);
 		
-	public function combine<In2, Out2, Combined>(other:Memo<In2, Out2>, f:Combiner<Out, Out2, Combined>, ?c1:Comparer<Out>, ?c2:Comparer<Out2>):Memo<Pair<In, In2>, Combined>
-		return new CombinedMemo(this, other, f, c1, c2);
+	public function combine<In2, Out2, Combined>(other:Memo<In2, Out2>, f:Combiner<Out, Out2, Combined>, ?c1:Comparer<Out>, ?c2:Comparer<Out2>):Memo<Pair<In, In2>, Combined> {
+		var comparer = switch [c1, c2] {
+			case [null, null]:
+				PairComparer.get();
+			default: 
+				if(c1 == null) c1 = EqualityComparer.get();
+				if(c2 == null) c2 = EqualityComparer.get();
+				new PairComparer(c1, c2);
+		}
+		return new CombinedMemo(this, other, f, comparer);
+	}
 		
 	
 }
@@ -28,41 +37,48 @@ interface MemoObject<In, Out> {
 
 typedef Cache<In, Out> = Pair<In, Promise<Out>>;
 
-class SimpleMemo<In, Out> implements MemoObject<In, Out> {
-
-	var f:Next<In, Out>;
-	var comparer:Comparer<In>;
-	var cache:Cache<In, Out>;
+class MemoBase<In, CIn, Out> implements MemoObject<In, Out> {
+	var comparer:Comparer<CIn>;
+	var cache:Cache<CIn, Out>;
 	
-	public function new(f, ?comparer) {
-		this.f = f;
+	public function new(?comparer:Comparer<CIn>)
 		this.comparer = comparer != null ? comparer : EqualityComparer.get();
-	}
 	
-	public function get(v:In):Promise<Out> {
-		if(cache == null || !comparer.equals(cache.a, v))
-			cache = new Pair(v, f(v));
-		return cache.b;
-	}
-	
+	public function get(v:In):Promise<Out>
+		throw 'abstract';
+		
 	public function invalidate()
 		cache = null;
 }
 
-class MapMemo<In, Mid, Out> implements MemoObject<In, Out> {
+class SimpleMemo<In, Out> extends MemoBase<In, In, Out> {
+
+	var f:Next<In, Out>;
+	
+	public function new(f, ?comparer) {
+		super(comparer);
+		this.f = f;
+	}
+	
+	override function get(v:In):Promise<Out> {
+		if(cache == null || !comparer.equals(cache.a, v))
+			cache = new Pair(v, f(v));
+		return cache.b;
+	}
+}
+
+class MapMemo<In, Mid, Out> extends MemoBase<In, Mid, Out> {
 
 	var prev:Memo<In, Mid>;
 	var f:Next<Mid, Out>;
-	var cache:Cache<Mid, Out>;
-	var comparer:Comparer<Mid>;
 	
 	public function new(prev, f, ?comparer) {
+		super(comparer);
 		this.prev = prev;
 		this.f = f;
-		this.comparer = comparer != null ? comparer : EqualityComparer.get();
 	}
 	
-	public function get(v:In):Promise<Out> {
+	override function get(v:In):Promise<Out> {
 		return prev.get(v).next(function(o) {
 			if(cache == null || !comparer.equals(cache.a, o))
 				cache = new Pair(o, f(o));
@@ -70,50 +86,27 @@ class MapMemo<In, Mid, Out> implements MemoObject<In, Out> {
 		});
 	}
 	
-	public function invalidate()
-		cache = null;
 }
 
-class CombinedMemo<In1, In2, Out1, Out2, Combined> implements MemoObject<Pair<In1, In2>, Combined> {
+class CombinedMemo<In1, In2, Out1, Out2, Combined> extends MemoBase<Pair<In1, In2>, Pair<Out1, Out2>, Combined> {
 
 	var m1:Memo<In1, Out1>;
 	var m2:Memo<In2, Out2>;
 	var f:Combiner<Out1, Out2, Combined>;
-	var cache:Cache<Pair<Out1, Out2>, Combined>;
-	var c1:Comparer<Out1>;
-	var c2:Comparer<Out2>;
 	
-	public function new(m1, m2, f, ?c1, ?c2) {
+	public function new(m1, m2, f, ?comparer) {
+		super(comparer);
 		this.m1 = m1;
 		this.m2 = m2;
 		this.f = f;
-		this.c1 = c1 != null ? c1 : EqualityComparer.get();
-		this.c2 = c2 != null ? c2 : EqualityComparer.get();
 	}
 	
-	public function get(v:Pair<In1, In2>):Promise<Combined> {
+	override function get(v:Pair<In1, In2>):Promise<Combined> {
 		return (m1.get(v.a) && m2.get(v.b))
 			.next(function(out) {
-				if(cache == null || !c1.equals(cache.a.a, out.a) || !c2.equals(cache.a.b, out.b))
+				if(cache == null || !comparer.equals(cache.a, out))
 					cache = new Pair(out, f(out.a, out.b));
 				return cache.b;
 			});
 	}
-	
-	public function invalidate()
-		cache = null;
-}
-
-@:callable
-abstract Combiner<In1, In2, Out>(In1->In2->Promise<Out>) from In1->In2->Promise<Out> {
-      
-  @:from static function ofSafe<In1, In2, Out>(f:In1->In2->Outcome<Out, Error>):Combiner<In1, In2, Out> 
-    return function (x1, x2) return f(x1, x2);
-    
-  @:from static function ofSync<In1, In2, Out>(f:In1->In2->Future<Out>):Combiner<In1, In2, Out> 
-    return function (x1, x2) return f(x1, x2);
-    
-  @:from static function ofSafeSync<In1, In2, Out>(f:In1->In2->Out):Combiner<In1, In2, Out> 
-    return function (x1, x2) return f(x1, x2);
-	
 }
